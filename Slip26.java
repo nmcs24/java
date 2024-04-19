361f44375cb3846610c5ff91e8093a21ce582c83
nmcs24 <166683363+nmcs24@users.noreply.github.com>
2024-04-19 22:26:59 +0530

    Update Slip26.java

--- Slip26.java
+++ Slip26.java
@@ -1,7 +1,4 @@
-//Q1) Define a Item class (item_number, item_name, item_price). Define a default and parameterized 
-//constructor. Keep a count of objects created. Create objects using parameterized constructor 
-//and display the object count after each object is created.(Use static member and method).Also 
-//display the contents of each object.
+//Q1
 class Item 
 {
     int item_number;
@@ -40,13 +37,9 @@ public class Slip26
         Item.displayObjectCount();
     }
 }
-/*
-Q2)Define a class ‘Donor’ to store the below mentioned details of a blood donor. name, age, 
-address, contactnumber, bloodgroup, date of last donation. Create ‘n’ objects of this class for
-all the regular donors at Pune. Write these objects to a file. Read these objects from the file and 
-display only those donors’ details whose blood group is ‘A+ve’ and had not donated for the
-recent six months.
-*/
+
+//Q2
+
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
